e05df3bbfa1f709c7aef8b11c4f1cf231fcada6e
Jonathan Scheiding <jscheiding@certicasolutions.com>
2019-04-28 09:19:59 -0400

    Breaking up the ebook into the report's major sections

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ import cheerio from 'cheerio';
 import fs from 'fs';
 import Epub from 'epub-gen';
 
-const content = fs.readFileSync('./input/input.html');
-const $ = cheerio.load(content);
+const inputHtml = fs.readFileSync('./input/input.html');
+const $ = cheerio.load(inputHtml);
 
-const allPages = $('.g-doc-page');
-const output = $('<div></div>');
-output.append(allPages.find('p'));
+const allPages = $('.g-doc-page').toArray();
+
+const sections = [
+  { name: "Volume I Cover", firstPage: 1, lastPage: 1 },
+  // { name: "Volume I TOC", firstPage: 3, lastPage: 7, isToc = true },
+  { name: "Volume I Introduction", firstPage: 9, lastPage: 11 },
+  { name: "Volume I Summary", firstPage: 12, lastPage: 18 },
+  { name: "Volume I", firstPage: 19, lastPage: 207 },
+  { name: "Volume II Cover", firstPage: 208, lastPage: 208 },
+  { name: "Volume II Introduction", firstPage: 213, lastPage: 214 },
+  { name: "Volume II Summary", firstPage: 215, lastPage: 220 }
+]
+
+const content = sections.map(
+  section => {
+    const html = $('<div></div>');
+    const pages = allPages.slice(section.firstPage - 1, section.lastPage - 1);
+    for(const page of pages) {
+      html.append($(page).find('.g-doc-html > *'));
+    }
+
+    return {
+      title: section.name,
+      data: html.html()
+    };
+  }
+)
 
 const options = {
   title: 'Mueller Report',
-  content: [
-    {
-      title: 'ALL OF IT',
-      data: output.html()
-    }
-  ]
+  content: content
 };
 
 new Epub(options, './output/output.epub');
